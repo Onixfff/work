6edcb33bd8f73d4fe5350cc52575dff5e40f150f
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-03-06 15:00:30 +0200

    Добавил в проект проект для ссылки

---
+++ Library/ClassLibraryGetIp/ClassLibraryGetIp/Main.cs
@@ -0,0 +1,80 @@
+﻿using ClassLibraryGetIp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibraryGetIp
+{
+    public class Main
+    {
+        private readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://192.168.100.100:5048")
+        };
+
+        public async Task<Ip> GetIp(string serverName)
+        {
+            Ip ip = new Ip();
+            string _errorMessage = "Ошибка";
+
+            try
+            {
+                var cancellationToken = new CancellationTokenSource();
+                cancellationToken.CancelAfter(TimeSpan.FromSeconds(30)); // Отмена через 30 секунд
+
+                // Формируем строку запроса
+                var requestUriString = $"/api/Macaddresstables/GetIpAsync?serverName={serverName}";
+
+                // Выполняем запрос
+                var response = await client.GetAsync(requestUriString, cancellationToken.Token);
+
+                // Проверяем успешность запроса
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+
+                    if (result.FirstOrDefault().Value != null)
+                    {
+                        ip.CreateError(result.FirstOrDefault().Value);
+                        return ip;
+                    }
+                    else
+                    {
+                        ip.CreateIp(result.FirstOrDefault().Key);
+                    }
+                }
+                else
+                {
+                    // Логируем ошибку HTTP
+                    _errorMessage = $"Ошибка HTTP-запроса: {(int)response.StatusCode} - {response.ReasonPhrase}";
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                _errorMessage = $"Ошибка HTTP-запроса: {ex.Message}";
+            }
+            catch (TaskCanceledException ex)
+            {
+                _errorMessage = $"Запрос был отменён (таймаут или отмена токеном)\n: {ex.Message}";
+                Console.WriteLine(_errorMessage);
+            }
+            catch(InvalidOperationException ex)
+            {
+                _errorMessage = $"{ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                _errorMessage = "Произошла неожиданная ошибка.";
+            }
+
+            ip.CreateError(_errorMessage);
+
+            return ip;
+        }
+    }
+}
---
+++ Library/ClassLibraryGetIp/ClassLibraryGetIp/Models/Ip.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassLibraryGetIp.Models
+{
+    public class Ip
+    {
+        private string _ip = default;
+        private string _error = default;
+
+        public Ip() { }
+
+        public Ip(string ip, string error)
+        {
+            _ip = ip;
+            _error = error;
+        }
+
+        public string GetIp()
+        {
+            if (_ip == null)
+            {
+                throw new InvalidOperationException("IP address is not set.");
+            }
+            return _ip;
+        }
+
+        public string GetError() 
+        {
+            if (_error == null) 
+            {
+                throw new InvalidOperationException("Error is not set");
+            }
+            return _error;
+        }
+
+        public void CreateError(string error)
+        {
+            if (_error == null)
+                _error = error;
+        }
+
+        public void CreateIp(string ip)
+        {
+            if (_ip == null)
+                _ip = ip;
+        }
+    }
+}
